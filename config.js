fdf66ea6586d287cb0356ade11aae94543434524
jiaowei94 <168520779+jiaowei94@users.noreply.github.com>
2026-07-28 10:07:50 +0800

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const CONFIG = {
   
   // Supabase 项目配置
   SUPABASE_URL: "https://zcvgirshnyqenkjknrci.supabase.co",
-  SUPABASE_ANON_KEY: "your-supabase-anon-key"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdmdpcnNobnlxZW5ramtucmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMDExNDcsImV4cCI6MjEwMDY3NzE0N30.TjKa3v6tnr4xANh5Z9vuf5SFESUpI8ZHq_dSIizl7fQ"
 };
